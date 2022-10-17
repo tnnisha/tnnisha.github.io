fd4a86e729da1a606375f80ca8163bb189221e73
tnnisha <115686326+tnnisha@users.noreply.github.com>
2022-10-17 15:34:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,12 +9,26 @@ class App{
 		this.camera.position.set(0,0,4);
 		this.scene=new THREE.Scene();
 		this.scene.background=new THREE.Color(0xaaaaaa);
+		
+		const ambient=new THREE.HemisphereLight(0xffffff,0xbbbbff,0.3);
+		this.scene.add(ambient);
+		
+		cont light=new THREE.DirectionalLight();
+		light.position.set(0.2,1,1);
+		this.scene.add(light);
+		
 		this.renderer =new THREE.WebGLRenderer({antialias :true});
 		this.renderer.setPixelRatio(window.devicePixelRatio);
 		this.renderer.setSize(window.innerWidth,window.innerHeight);
 		container.appendChild(this.renderer.domElement);
 		this.renderer.setAnimationLoop(this.render.bind(this));
-	        window.addEventListener('resize', this.resize.bind(this) );
+	        
+		const geometry=new THREE.BoxBufferGeometry();
+		const material=new THREE.MeshStandardMaterial({color :0xff0000});
+		this.mesh=new THREE.Mesh(geometry,material);
+		this.scene.add(this.mesh);
+		
+		window.addEventListener('resize', this.resize.bind(this) );
 	}	
     
     resize(){
@@ -22,7 +36,8 @@ class App{
     }
     
 	render( ) {  
-        this.renderer.render(this.scene,this.camera);
+		this.mesh.rotateY(0.01);
+		this.renderer.render(this.scene,this.camera);
     }
 }
 
