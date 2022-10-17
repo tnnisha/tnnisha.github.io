a1e3fca82596bc8dbc106ba0284d26d01f9bd073
tnnisha <115686326+tnnisha@users.noreply.github.com>
2022-10-17 16:31:28 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -5,30 +5,9 @@ class App{
 	constructor(){
 		const container = document.createElement( 'div' );
 		document.body.appendChild( container );
-		this.camera=new THREE.PerspectiveCamera(60,window.innerWidth/window.innerHeight,0.1,100);
-		this.camera.position.set(0,0,4);
-		this.scene=new THREE.Scene();
-		this.scene.background=new THREE.Color(0xaaaaaa);
-		
-		const ambient=new THREE.HemisphereLight(0xffffff,0xbbbbff,0.3);
-		this.scene.add(ambient);
-		
-		cont light=new THREE.DirectionalLight();
-		light.position.set(0.2,1,1);
-		this.scene.add(light);
-		
-		this.renderer =new THREE.WebGLRenderer({antialias :true});
-		this.renderer.setPixelRatio(window.devicePixelRatio);
-		this.renderer.setSize(window.innerWidth,window.innerHeight);
-		container.appendChild(this.renderer.domElement);
-		this.renderer.setAnimationLoop(this.render.bind(this));
-	        
-		const geometry=new THREE.BoxBufferGeometry();
-		const material=new THREE.MeshStandardMaterial({color :0xff0000});
-		this.mesh=new THREE.Mesh(geometry,material);
-		this.scene.add(this.mesh);
-		
-		window.addEventListener('resize', this.resize.bind(this) );
+    
+        
+        window.addEventListener('resize', this.resize.bind(this) );
 	}	
     
     resize(){
@@ -36,8 +15,7 @@ class App{
     }
     
 	render( ) {  
-		this.mesh.rotateY(0.01);
-		this.renderer.render(this.scene,this.camera);
+        
     }
 }
 
